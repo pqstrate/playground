67937e0d5497b345d83e787e704d949ed6a07d19
yaps <allaboutshop3@163.com>
2025-09-12 12:11:24 -0400

    update bench script

--- bench.sh
+++ bench.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
-RUST_LOG=trace NUM_THREADS=1 cargo run --release --bin p3_demo > p3_1_thread.log
-RUST_LOG=trace NUM_THREADS=1 cargo run --release --bin wf_demo > wf_1_thread.log
+RUSTFLAGS="-C target-feature=+avx512f" RUST_LOG=trace NUM_THREADS=1 cargo run --release --bin p3_demo > 1_thread_p3_avx512.log
+RUST_LOG=trace NUM_THREADS=1 cargo run --release --bin p3_demo > 1_thread_p3.log
+RUST_LOG=trace NUM_THREADS=1 cargo run --release --bin wf_demo > 1_thread_wf.log
+
+RUSTFLAGS="-C target-feature=+avx512f" RUST_LOG=trace NUM_THREADS=2 cargo run --release --bin p3_demo > 2_thread_p3_avx512.log
+RUST_LOG=trace NUM_THREADS=2 cargo run --release --bin p3_demo > 2_thread_p3.log
+RUST_LOG=trace NUM_THREADS=2 cargo run --release --bin wf_demo > 2_thread_wf.log
 
 
-RUST_LOG=trace NUM_THREADS=2 cargo run --release --bin p3_demo > p3_2_thread.log
-RUST_LOG=trace NUM_THREADS=2 cargo run --release --bin wf_demo > wf_2_thread.log
+RUSTFLAGS="-C target-feature=+avx512f" RUST_LOG=trace NUM_THREADS=4 cargo run --release --bin p3_demo > 4_thread_p3_avx512.log
+RUST_LOG=trace NUM_THREADS=4 cargo run --release --bin p3_demo > 4_thread_p3.log
+RUST_LOG=trace NUM_THREADS=4 cargo run --release --bin wf_demo > 4_thread_wf.log
 
-RUST_LOG=trace NUM_THREADS=4 cargo run --release --bin p3_demo > p3_4_thread.log
-RUST_LOG=trace NUM_THREADS=4 cargo run --release --bin wf_demo > wf_4_thread.log
+RUSTFLAGS="-C target-feature=+avx512f" RUST_LOG=trace NUM_THREADS=8 cargo run --release --bin p3_demo > 8_thread_p3_avx512.log
+RUST_LOG=trace NUM_THREADS=8 cargo run --release --bin p3_demo > 8_thread_p3.log
+RUST_LOG=trace NUM_THREADS=8 cargo run --release --bin wf_demo > 8_thread_wf.log
 
-RUST_LOG=trace NUM_THREADS=8 cargo run --release --bin p3_demo > p3_8_thread.log
-RUST_LOG=trace NUM_THREADS=8 cargo run --release --bin wf_demo > wf_8_thread.log
-
-RUST_LOG=trace NUM_THREADS=16 cargo run --release --bin p3_demo > p3_16_thread.log
-RUST_LOG=trace NUM_THREADS=16 cargo run --release --bin wf_demo > wf_16_thread.log
+RUSTFLAGS="-C target-feature=+avx512f" RUST_LOG=trace NUM_THREADS=16 cargo run --release --bin p3_demo > 16_thread_p3_avx512.log
+RUST_LOG=trace NUM_THREADS=16 cargo run --release --bin p3_demo > 16_thread_p3.log
+RUST_LOG=trace NUM_THREADS=16 cargo run --release --bin wf_demo > 16_thread_wf.log
