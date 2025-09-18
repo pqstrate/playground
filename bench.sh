c33fea9f6b7c118ae9cf3acc18c5c68a98d5e813
yaps <allaboutshop3@163.com>
2025-09-18 10:06:36 -0400

    fix bug with p3-blake

--- bench.sh
+++ bench.sh
@@ -17,22 +17,22 @@
 # NUM_THREADS=16 HASH_TYPE=poseidon2 cargo run --release --bin p3_monty_demo > 16_thread_p3_monty_poseidon2.log
 
 ######################################
-# P3 demos with Keccak hash function
+# P3 demos with blake3 hash function
 ######################################
-RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=1 HASH_TYPE=keccak cargo run --release --bin p3_demo > 1_thread_p3_blake3_avx512.log
-NUM_THREADS=1 HASH_TYPE=keccak cargo run --release --bin p3_demo > 1_thread_p3_blake3.log
+RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=1 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 1_thread_p3_blake3_avx512.log
+NUM_THREADS=1 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 1_thread_p3_blake3.log
 
-# RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=2 HASH_TYPE=keccak cargo run --release --bin p3_demo > 2_thread_p3_blake3_avx512.log
-# NUM_THREADS=2 HASH_TYPE=keccak cargo run --release --bin p3_demo > 2_thread_p3_blake3.log
+# RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=2 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 2_thread_p3_blake3_avx512.log
+# NUM_THREADS=2 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 2_thread_p3_blake3.log
 
-# RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=4 HASH_TYPE=keccak cargo run --release --bin p3_demo > 4_thread_p3_blake3_avx512.log
-# NUM_THREADS=4 HASH_TYPE=keccak cargo run --release --bin p3_demo > 4_thread_p3_blake3.log
+# RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=4 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 4_thread_p3_blake3_avx512.log
+# NUM_THREADS=4 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 4_thread_p3_blake3.log
 
-# RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=8 HASH_TYPE=keccak cargo run --release --bin p3_demo > 8_thread_p3_blake3_avx512.log
-# NUM_THREADS=8 HASH_TYPE=keccak cargo run --release --bin p3_demo > 8_thread_p3_blake3.log
+# RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=8 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 8_thread_p3_blake3_avx512.log
+# NUM_THREADS=8 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 8_thread_p3_blake3.log
 
-RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=16 HASH_TYPE=keccak cargo run --release --bin p3_demo > 16_thread_p3_blake3_avx512.log
-NUM_THREADS=16 HASH_TYPE=keccak cargo run --release --bin p3_demo > 16_thread_p3_blake3.log
+RUSTFLAGS="-C target-feature=+avx512f" NUM_THREADS=16 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 16_thread_p3_blake3_avx512.log
+NUM_THREADS=16 HASH_TYPE=blake3 cargo run --release --bin p3_demo > 16_thread_p3_blake3.log
 
 ######################################
 # P3 demos with Keccak hash function
